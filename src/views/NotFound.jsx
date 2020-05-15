5df4e23fa2a2cae957b0558b375e5a7a72444285
AndreeaIsabela <bala.andreea@yahoo.com>
2020-05-15 23:57:42 +0300

    [master] Add 404 page.

---
+++ src/views/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="container text-center"> 
+    <h1> ERROR 404</h1>
+    <h3 className="text-muted">Page not found</h3>
+  </div>
+);
+
+export default NotFound;
+
