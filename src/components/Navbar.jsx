3ee3794f654d4396aea4cc17ecba5eb6a48a3ea5
AndreeaIsabela <bala.andreea@yahoo.com>
2020-05-18 00:53:52 +0300

    [master] Add navbar style.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <div>
-      <Link to="/">Home </Link>
-      <Link to="/dashboard">Dashboard </Link>
-    </div>
+  <ul className="nav nav-tabs justify-content-end">
+     <li className="nav-item">
+      <Link className="nav-link active" to="/">Home </Link>
+     </li>
+     <li className="nav-item">
+      <Link className="nav-link" to="/dashboard">Dashboard </Link>
+     </li>
+  </ul>
 );
 
 export default Navbar;
