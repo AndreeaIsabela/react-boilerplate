2f23c9b38c36b8e3b53061c0cf2e7d4ed2bb5378
AndreeaIsabela <bala.andreea@yahoo.com>
2020-05-18 00:54:33 +0300

    [master] Add redux store state.

---
+++ src/redux-store/state.js
@@ -0,0 +1,16 @@
+export const state = {
+  tasks: [
+    {
+      _id: 1,
+      name: 'task1',
+    },
+    {
+      _id: 2,
+      name: 'task2',
+    },
+    {
+      _id: 3,
+      name: 'task3',
+    },
+  ],
+};
